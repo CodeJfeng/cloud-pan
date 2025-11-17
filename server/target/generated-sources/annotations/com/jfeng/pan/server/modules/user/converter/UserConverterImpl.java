f99c5000d78496af95b80358aa2bd78048a45ab1
CodeJfeng <1683770168@qq.com>
2025-11-17 10:33:12 +0800

    add 增加用户的字段映射转换器，使用mapstruct组件自动绑定生成

---
+++ server/target/generated-sources/annotations/com/jfeng/pan/server/modules/user/converter/UserConverterImpl.java
@@ -0,0 +1,47 @@
+package com.jfeng.pan.server.modules.user.converter;
+
+import com.jfeng.pan.server.modules.user.context.UserRegisterContext;
+import com.jfeng.pan.server.modules.user.entity.RPanUser;
+import com.jfeng.pan.server.modules.user.po.UserRegisterPO;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-11-17T10:30:57+0800",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.9 (Microsoft)"
+)
+@Component
+public class UserConverterImpl implements UserConverter {
+
+    @Override
+    public UserRegisterContext userRegisterPO2UserRegisterContext(UserRegisterPO userRegisterPO) {
+        if ( userRegisterPO == null ) {
+            return null;
+        }
+
+        UserRegisterContext userRegisterContext = new UserRegisterContext();
+
+        userRegisterContext.setUsername( userRegisterPO.getUsername() );
+        userRegisterContext.setPassword( userRegisterPO.getPassword() );
+        userRegisterContext.setQuestion( userRegisterPO.getQuestion() );
+        userRegisterContext.setAnswer( userRegisterPO.getAnswer() );
+
+        return userRegisterContext;
+    }
+
+    @Override
+    public RPanUser userRegisterContext2RPanUser(UserRegisterContext userRegisterContext) {
+        if ( userRegisterContext == null ) {
+            return null;
+        }
+
+        RPanUser rPanUser = new RPanUser();
+
+        rPanUser.setUsername( userRegisterContext.getUsername() );
+        rPanUser.setQuestion( userRegisterContext.getQuestion() );
+        rPanUser.setAnswer( userRegisterContext.getAnswer() );
+
+        return rPanUser;
+    }
+}
