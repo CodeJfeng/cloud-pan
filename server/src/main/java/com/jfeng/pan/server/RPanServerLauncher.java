2e811e0d9c8dcba6823e3fe58f36550fc064b09c
CodeJfeng <1683770168@qq.com>
2026-02-12 11:31:10 +0800

    feat(share): 监听文件状态变更自动更新分享链接状态 ## 背景 当文件被删除或从回收站还原时，已生成的分享链接仍可能被访问，导致用户能够操作已删除的文件或无法访问已还原的文件，存在数据安全隐患与体验问题。 ## 功能概述 实现文件状态变更与分享状态的联动机制，通过事件驱动方式自动更新相关分享链接的有效性状态。 - 通过监听文件删除和还原事件,执行对应的刷新逻辑

--- server/src/main/java/com/jfeng/pan/server/RPanServerLauncher.java
+++ server/src/main/java/com/jfeng/pan/server/RPanServerLauncher.java
@@ -1,10 +1,13 @@
 package com.jfeng.pan.server;
 
 import com.jfeng.pan.core.constants.RPanConstants;
+//import com.jfeng.pan.server.common.stream.channel.PanChannel;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+//import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 
@@ -13,6 +16,8 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @ServletComponentScan(basePackages = RPanConstants.BASE_COMPONENT_SCAN_PATH)
 @EnableTransactionManagement
 @MapperScan(basePackages =  RPanConstants.BASE_COMPONENT_SCAN_PATH+".server.modules.**.mapper")
+@EnableAsync
+//@EnableBinding(PanChannel.class)
 public class RPanServerLauncher {
     public static void main(String[] args) {
         SpringApplication.run(RPanServerLauncher.class, args);
